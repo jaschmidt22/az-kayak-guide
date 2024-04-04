e10e582c7cd796e2cccb1333b0ac45e67b4e748d
jaschmidt22 <146290812+jaschmidt22@users.noreply.github.com>
2024-04-04 09:38:32 -0700

    gitignore

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,54 @@
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(name: $username) {
+      _id
+      username
+      email
+      blogposts
+    }
+  }
+`;
+export const ADD_BLOGPOST = gql`
+  mutation addBlogPost(
+    $blogPostText: String!
+    $blogPostAuthor: String!
+    $createdAt: Date!
+  ) {
+    addBlogPost(
+      blogPostText: $blogPostText
+      blogPostAuthor: $blogPostAuthor
+      createdAt: $createdAt
+    ) {
+      _id
+      blogPostText
+      blogPostAuthor
+      createdAt
+    }
+  }
+`;
+export const REMOVE_BLOGPOST = gql`
+  mutation removeBlogPost($blogPostId: ID!) {
+    removeBlogPost(blogPostId: $blogPostId) {
+      _id
+      success
+      message
+    }
+  }
+`;
+export const UPDATE_BLOGPOST = gql`
+  mutation updateBlogPost(
+    $blogPostId: ID!
+    $updatedText: String!
+    $updatedAt: Date!
+  ) {
+    updateBlogPost(
+      blogPostId: $blogPostId
+      updatedText: $updatedText
+      updatedAt: $updatedAt
+    ) {
+      _id
+      blogPostText
+      updatedAt
+    }
+  }
+`;
