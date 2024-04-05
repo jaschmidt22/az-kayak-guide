ff39dfd85cc6523522d0477608fe0f637210676b
Zach <zdgemmill@gmail.com>
2024-04-04 19:44:24 -0700

    Mutations

--- server/models/BlogPost.js
+++ server/models/BlogPost.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require("mongoose");
-const dateFormat = require("../utils/dateFormat");
+
 
 const blogPostSchema = new Schema({
   blogPostText: {
@@ -14,11 +14,11 @@ const blogPostSchema = new Schema({
     required: true,
     trim: true,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (timestamp) => dateFormat(timestamp),
-  },
+  // createdAt: {
+  //   type: Date,
+  //   default: Date.now,
+  //   get: (timestamp) => dateFormat(timestamp),
+  // },
   //   comments: [
   //     {
   //       commentText: {
