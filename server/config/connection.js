db0969f785cce049b46ebfa1ddce133a44a5e81a
jaschmidt22 <146290812+jaschmidt22@users.noreply.github.com>
2024-04-09 12:25:42 -0700

    testdeploy2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb+srv://jessicaschmidt22:Je$$1ca80@cluster0.spuxllz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true"
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/az-kayak-guide"
 );
 
 module.exports = mongoose.connection;
 
-// mongodb+srv://jessicaschmidt22:<Je$$1ca80>@cluster0.spuxllz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
-// mongodb+srv://jessicaschmidt22:<password>@cluster0.spuxllz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+
+
+mongodb+srv://jessicaschmidt22:<password>@cluster0.spuxllz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
