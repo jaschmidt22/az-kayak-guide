20565cfa54e722a7e5d2721e48eb5f41a9fe3fad
jaschmidt22 <146290812+jaschmidt22@users.noreply.github.com>
2024-04-09 14:57:58 -0700

    connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://jessicaschmidt22:Sammi226@cluster0.spuxllz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://jessicaschmidt22:Sammi226@cluster0.spuxllz.mongodb.net/az-kayak-guide?retryWrites=true&w=majority&appName=Cluster0"
 );
 
 module.exports = mongoose.connection;
 
-//mongodb+srv://jessicaschmidt22:Sammi226@cluster0.spuxllz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+//mongodb+srv://jessicaschmidt22:Sammi226@cluster0.spuxllz.mongodb.net/az-kayak-guide?retryWrites=true&w=majority&appName=Cluster0
