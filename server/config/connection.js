adfefcc549f19fe84e2830c736d0ff221a6e0e69
jaschmidt22 <146290812+jaschmidt22@users.noreply.github.com>
2024-04-09 10:35:39 -0700

    test deploy2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/az-kayak-guide"
+  process.env.MONGODB_URI ||
+    "mongodb+srv://jessicaschmidt22:Je$$1ca80@cluster0.spuxllz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    useNewUrlParser: true, // Add this option to avoid useNewUrlParser deprecation warning
+    useUnifiedTopology: true, // Add this option to avoid useUnifiedTopology deprecation warning
+  }
 );
 
 module.exports = mongoose.connection;
+
+// mongodb+srv://jessicaschmidt22:<Je$$1ca80>@cluster0.spuxllz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+// mongodb+srv://jessicaschmidt22:<password>@cluster0.spuxllz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
