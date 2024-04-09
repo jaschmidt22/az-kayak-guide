4659a7c7d4af32573263f801fb6438fa20a83afe
jaschmidt22 <146290812+jaschmidt22@users.noreply.github.com>
2024-04-09 10:51:05 -0700

    testdeploy2

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://jessicaschmidt22:Je$$1ca80@cluster0.spuxllz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://jessicaschmidt22:Je$$1ca80@cluster0.spuxllz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tls=true",
   {
     useNewUrlParser: true, // Add this option to avoid useNewUrlParser deprecation warning
     useUnifiedTopology: true, // Add this option to avoid useUnifiedTopology deprecation warning
